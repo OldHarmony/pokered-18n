cad7608a72b3f3ffe7fd15c08e74e5291f0d1d78
OldHarmony <83431840+OldHarmony@users.noreply.github.com>
2025-07-14 01:40:34 +0200

    paths for the edition gfx was not on the new place

--- gfx/versions/german.asm
+++ gfx/versions/german.asm
@@ -1,8 +1,8 @@
 Version_GFX:
 IF DEF(_RED)
-	INCBIN "gfx/title/red_version.1bpp" ; 10 tiles
+	INCBIN "gfx/title/german/red_version.1bpp" ; 10 tiles
 ENDC
 IF DEF(_BLUE)
-	INCBIN "gfx/title/blue_version.1bpp" ; 10 tiles
+	INCBIN "gfx/title/german/blue_version.1bpp" ; 10 tiles
 ENDC
 Version_GFXEnd:
